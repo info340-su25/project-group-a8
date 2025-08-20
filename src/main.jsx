8ed8bfa8f8edc6bcbbf428e85d69ccd1da743ad9
fiona-23 <fionaevans0131@gmail.co>
2025-08-20 11:09:00 -0700

    added firebase configuration

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,25 @@ import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5fo8uFz3xpSIaYI-7rPOBhAJJEVy_C2w",
+  authDomain: "thrive-groupa8.firebaseapp.com",
+  projectId: "thrive-groupa8",
+  storageBucket: "thrive-groupa8.firebasestorage.app",
+  messagingSenderId: "296815397597",
+  appId: "1:296815397597:web:f68b39b831fcd0ebb52934"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
